d784caf0df502f75f915e89d9b75396774a8e033
hein soe <heinsoe@agga.io>
2022-03-17 19:47:45 +0630

    add response code

--- app/Http/Controllers/DynamicFormController.php
+++ app/Http/Controllers/DynamicFormController.php
@@ -7,6 +7,7 @@ use App\Models\SurveysForm;
 use App\Models\DynamicForm;
 use App\Events\SurveysFormSubmitEvent;
 use Event;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class DynamicFormController extends Controller
@@ -19,12 +20,12 @@ class DynamicFormController extends Controller
                 $surveyForm->form_fields = json_encode($request->all());
                 $result = $surveyForm->save();
             }
-            
+
             if($result){
-                
+
                 $receiver_name = $request->name;
                 $receiver_email = $request->email;
-                
+
                 // Mail::to($receiver_email)->send(new SurveysFormSubmitMailer($receiver_name, $receiver_email));
 
                 Event::dispatch(new SurveysFormSubmitEvent($receiver_name, $receiver_email));
@@ -38,21 +39,23 @@ class DynamicFormController extends Controller
             return response()->json([
                 'msg' => $e->getMessage(),
                 'status' => 500
-            ]);
+            ])
+            ->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR, Response::$statusTexts[Response::HTTP_INTERNAL_SERVER_ERROR]);
         }
     }
 
     public function df_update(Request $request)
     {
         try {
-            
+
             $df_form = DynamicForm::where('id', $request->id)->first();
-            
+
             if($df_form->active == $request->status){
                 return response()->json([
                     "msg" => "Same Status",
                     "status" => 400
-                ]);
+                ])
+                ->setStatusCode(Response::HTTP_BAD_REQUEST, Response::$statusTexts[Response::HTTP_BAD_REQUEST]);
             }
 
             $df_form->active = $request->status == 'true' ? 1 : 0;
@@ -61,13 +64,14 @@ class DynamicFormController extends Controller
             return response()->json([
                 "msg" => "Update Success",
                 "status" => 200
-            ]);
+            ])->setStatusCode(Response::HTTP_OK, Response::$statusTexts[Response::HTTP_OK]);
 
         } catch (\Exception $e) {
             return response()->json([
                 'msg' => $e->getMessage(),
                 'status' => 500
-            ]);
+            ])
+            ->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR, Response::$statusTexts[Response::HTTP_INTERNAL_SERVER_ERROR]);
         }
     }
 }
